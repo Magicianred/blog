026d491f543fee9833c7dbdec39265a191d7a440
Davi Michaelsen Mergener <davimichaelsenm@gmail.com>
2021-04-27 15:53:52 -0300

    Slider passado para o jquery

--- js/script.js
+++ js/script.js
@@ -2,58 +2,54 @@ $(document.body).ready(function(){
     let tempSlider = setInterval(trocaSlider, 5500)
 
     function trocaSlider(){
-        if(document.querySelector("#img1").classList.contains("block")){
-            document.querySelector("#img1").classList.remove("block")
-            document.querySelector("#img2").classList.add("block")
-        }else if(document.querySelector("#img2").classList.contains("block")){
-            document.querySelector("#img2").classList.remove("block")
-            document.querySelector("#img3").classList.add("block")
-        }else if(document.querySelector("#img3").classList.contains("block")){
-            document.querySelector("#img3").classList.remove("block")
-            document.querySelector("#img4").classList.add("block")
-        }else if(document.querySelector("#img4").classList.contains("block")){
-            document.querySelector("#img4").classList.remove("block")
-            document.querySelector("#img1").classList.add("block")
+        if($("#img1").hasClass("block")){
+            $("#img1").removeClass("block")
+            $("#img2").addClass("block")
+        }else if($("#img2").hasClass("block")){
+            $("#img2").removeClass("block")
+            $("#img3").addClass("block")
+        }else if($("#img3").hasClass("block")){
+            $("#img3").removeClass("block")
+            $("#img4").addClass("block")
+        }else if($("#img4").hasClass("block")){
+            $("#img4").removeClass("block")
+            $("#img1").addClass("block")
         }
     }
-    
-    document.querySelector("#blocoSlider").addEventListener("mouseover", function(){
-        clearInterval(tempSlider)
-        document.querySelector("#voltar").style.display = "flex"
-        document.querySelector("#avancar").style.display = "flex"
-    })
-    
-    document.querySelector("#blocoSlider").addEventListener("mouseout", function(){
+
+    $("#voltar").click(function(){
         clearInterval(tempSlider)
         tempSlider = setInterval(trocaSlider, 5500)
-        document.querySelector("#voltar").style.display = "none"
-        document.querySelector("#avancar").style.display = "none"
+        if($("#img1").hasClass("block")){
+            $("#img1").removeClass("block")
+            $("#img4").addClass("block")
+        }else if($("#img2").hasClass("block")){
+            $("#img2").removeClass("block")
+            $("#img1").addClass("block")
+        }else if($("#img3").hasClass("block")){
+            $("#img3").removeClass("block")
+            $("#img2").addClass("block")
+        }else if($("#img4").hasClass("block")){
+            $("#img4").removeClass("block")
+            $("#img3").addClass("block")
+        }
     })
-
-    document.querySelector("#avancar").addEventListener("click", function(){
+    
+    $("#avancar").click(function(){
         clearInterval(tempSlider)
         tempSlider = setInterval(trocaSlider, 5500)
         trocaSlider()
     })
-
-    document.querySelector("#voltar").addEventListener("click", function(){
+    
+    $("#blocoSlider").hover(function(){
         clearInterval(tempSlider)
-        if(document.querySelector("#img1").classList.contains("block")){
-            document.querySelector("#img1").classList.remove("block")
-            document.querySelector("#img4").classList.add("block")
-            tempSlider = setInterval(trocaSlider, 5500)
-        }else if(document.querySelector("#img2").classList.contains("block")){
-            document.querySelector("#img2").classList.remove("block")
-            document.querySelector("#img1").classList.add("block")
-            tempSlider = setInterval(trocaSlider, 5500)
-        }else if(document.querySelector("#img3").classList.contains("block")){
-            document.querySelector("#img3").classList.remove("block")
-            document.querySelector("#img2").classList.add("block")
-            tempSlider = setInterval(trocaSlider, 5500)
-        }else if(document.querySelector("#img4").classList.contains("block")){
-            document.querySelector("#img4").classList.remove("block")
-            document.querySelector("#img3").classList.add("block")
-            tempSlider = setInterval(trocaSlider, 5500)
-        }
-    }) 
+        $("#voltar").css("display", "flex")
+        $("#avancar").css("display", "flex")
+    }, function(){
+        clearInterval(tempSlider)
+        tempSlider = setInterval(trocaSlider, 5500)
+        $("#voltar").css("display", "none")
+        $("#avancar").css("display", "none")
+    })  
+  
 })

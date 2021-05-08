8c006047b615f91f2810fbcf509e8629f57a82bc
Davi Michaelsen Mergener <davimichaelsenm@gmail.com>
2021-05-08 15:41:34 -0300

    commit

--- js/home.js
+++ js/home.js
@@ -10,16 +10,13 @@ $(document.body).ready(function(){
 
     //Resumos de artigos
     $("#resumo1").load("artigos/artigo1.html #resArt")
-    if(!$("div").hasClass("blocoArtigo")){
-        $.ajax({
-            async: true,
-            type: 'GET',
-            url: "artigos/artigo1.html",
-            success: function(result){
-                
-            }
-        })
-    }
+    $("#resumo2").load("artigos/artigo2.html #resArt2")
+    $("#resumo3").load("artigos/artigo3.html #resArt3")
+    $("#resumo4").load("artigos/artigo4.html #resArt4")
+    $("#resumo5").load("artigos/artigo5.html #resArt5")
+    $("#resumo6").load("artigos/artigo6.html #resArt6")
+    $("#resumo7").load("artigos/artigo7.html #resArt7")
+   
 
   
    
